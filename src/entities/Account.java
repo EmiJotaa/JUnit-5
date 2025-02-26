e1516fb68f58efab61fba0e266d1a4db7d7c7522
Marco Júnior <mrcez4rr@gmail.com>
2025-02-26 10:45:43 -0300

    entitie Account

---
+++ src/entities/Account.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class Account {
+
+	private Long id;
+	private Double balance;
+	
+	public Account() {
+	}
+
+	public Account(Long id, Double balance) {
+		super();
+		this.id = id;
+		this.balance = balance;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+}
