43c04a3a209fb8c54402edc24a8bbb9178e6d9e9
Marco Júnior <mrcez4rr@gmail.com>
2025-02-26 10:52:00 -0300

    Logicas para deposit and withdraw

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,6 +1,8 @@
 package entities;
 
 public class Account {
+	
+	public static double DEPOSIT_FEE_PERCENTAGE = 0.02;
 
 	private Long id;
 	private Double balance;
@@ -24,5 +26,25 @@ public class Account {
 
 	public Double getBalance() {
 		return balance;
+	}
+	
+	public void deposit(double amount) {
+		if (amount > 0) {
+			amount -= amount * DEPOSIT_FEE_PERCENTAGE;
+			balance += amount;
+		}
+	}
+	
+	public void withdraw(double amount) {
+		if (amount > balance) {
+			throw new IllegalArgumentException();
+		}
+		balance -= amount;
+	}
+	
+	public double fullWithdraw() {
+		double aux = balance;
+		balance = 0.0;
+		return aux;
 	}
 }
